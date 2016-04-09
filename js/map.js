fbfcee98e9ae21d366c06fa26c5372b892774dc9
Javier Losa Balsa <javier.losa.balsa@udc.es>
2016-04-09 17:09:56 +0200

    Commit inicial

---
+++ js/map.js
@@ -0,0 +1,96 @@
+var app = angular.module('AngularGoogleMap', ['google-maps']);
+
+app.factory('MarkerCreatorService', function () {
+
+    var markerId = 0;
+
+    function create(latitude, longitude) {
+        var marker = {
+            options: {
+                animation: 1,
+                labelAnchor: "28 -5",
+                labelClass: 'markerlabel'    
+            },
+            latitude: latitude,
+            longitude: longitude,
+            id: ++markerId          
+        };
+        return marker;        
+    }
+
+    function invokeSuccessCallback(successCallback, marker) {
+        if (typeof successCallback === 'function') {
+            successCallback(marker);
+        }
+    }
+
+    function createByCoords(latitude, longitude, successCallback) {
+        var marker = create(latitude, longitude);
+        invokeSuccessCallback(successCallback, marker);
+    }
+
+    function createByAddress(address, successCallback) {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'address' : address}, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                var firstAddress = results[0];
+                var latitude = firstAddress.geometry.location.lat();
+                var longitude = firstAddress.geometry.location.lng();
+                var marker = create(latitude, longitude);
+                invokeSuccessCallback(successCallback, marker);
+            } else {
+                alert("Unknown address: " + address);
+            }
+        });
+    }
+
+    return {
+        createByCoords: createByCoords,
+        createByAddress: createByAddress
+    };
+
+});
+
+app.controller('MapCtrl', ['MarkerCreatorService', '$scope', function (MarkerCreatorService, $scope) {
+
+        MarkerCreatorService.createByCoords(43.3415225, -8.4477031, function (marker) {
+            marker.options.labelContent = 'Autentia';
+            $scope.autentiaMarker = marker;
+        });
+        
+        $scope.address = '';
+
+        $scope.map = {
+            center: {
+                latitude: $scope.autentiaMarker.latitude,
+                longitude: $scope.autentiaMarker.longitude
+            },
+            zoom: 12,
+            markers: [],
+            control: {},
+            options: {
+                scrollwheel: true
+            }
+        };
+
+        $scope.map.markers.push($scope.autentiaMarker);
+        
+        $scope.addAddress = function() {
+            var address = $scope.address;
+            if (address !== '') {
+                MarkerCreatorService.createByAddress(address, function(marker) {
+                	marker.options.labelContent = 'Taxi';
+                    $scope.map.markers.push(marker);
+                    refresh(marker);
+                });
+            }
+        };
+
+        function refresh(marker) {
+            $scope.map.control.refresh({latitude: marker.latitude,
+                longitude: marker.longitude});
+        }
+
+    }]);
+
+
